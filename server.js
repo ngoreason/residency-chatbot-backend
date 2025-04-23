db7a8ef6b2f84441d241dec2ce48e4c9c62f63a7
ngoreason <brandonngomed@gmail.com>
2025-04-22 19:41:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/query', async (req, res) => {
 
 app.post('/chat', async (req, res) => {
   const sessionId = uuid.v4();
-  const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);}
+  const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
 
   const request = {
     session: sessionPath,
@@ -56,6 +56,7 @@ app.post('/chat', async (req, res) => {
       },
     },
   };
+}
 
   try {
     const responses = await sessionClient.detectIntent(request);
